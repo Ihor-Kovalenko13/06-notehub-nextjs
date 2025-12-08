c082309a4cf47e241a6a2af5c6ea7b5b8b040a9c
Igor <ihor.kovalenko.01@gmail.com>
2025-12-08 13:13:26 +0100

    Fixed after mentor chek

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import axios from 'axios';
 import type { Note } from '@/types/note';
 
@@ -18,7 +20,8 @@ interface createNoteProps {
   tag: string;
 }
 
-const myToken = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+const myToken = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN as string;
+
 axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
 axios.defaults.headers.common['Authorization'] = `Bearer ${myToken}`;
 
@@ -34,21 +37,17 @@ export const fetchNotes = async ({
     },
   };
 
-  const response = await axios
-    .get<fetchNotesResponse>('/notes', options)
-    .then(response => response.data);
-  return response;
+  const response = await axios.get<fetchNotesResponse>('/notes', options);
+  return response.data;
 };
 
 export async function fetchNoteById(id: Note['id']): Promise<Note> {
-  const response = await axios
-    .get<Note>(`/notes/${id}`)
-    .then(response => response.data);
-  return response;
+  const response = await axios.get<Note>(`/notes/${id}`);
+  return response.data;
 }
 
 export const createNote = async (data: createNoteProps) => {
-  const response = await axios.post<Note>('/notes', data, {});
+  const response = await axios.post<Note>('/notes', data);
   return response.data;
 };
 
